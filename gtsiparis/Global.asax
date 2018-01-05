97b7dc6ca5c71fa6419c441f85732c0d79b7d91f
onurku <onurku@gmail.com>
2018-01-05 16:40:21 +0300

    Add project files.

---
+++ gtsiparis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gtsiparis.MvcApplication" Language="C#" %>
---
+++ gtsiparis/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gtsiparis.Startup))]
+namespace gtsiparis
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
